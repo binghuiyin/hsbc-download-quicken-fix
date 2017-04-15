4641c4f1304b88c286a99bf4d8bfd08b42aa38de
David Yin <bing.h.yin@gmail.com>
2017-04-15 03:45:35 -0700

    Add comments

--- fixquicken.php
+++ fixquicken.php
@@ -8,7 +8,7 @@
 <body>
 <?php
 
-
+// Input file name
 $filepath = "./TransHist.qfx";
 
 $fp = fopen($filepath, "r") or die("Couldn't open $filename");
@@ -16,36 +16,36 @@ $buf ='';
 $counter = 0;
 while (!feof($fp)) {
    $line = fgets($fp, 1024);
-   
+   	// Trim DTPOSTED to 10 digits only, then give it to FITID, if FITID is empty
    	if (strpos($line, '<DTPOSTED>') !== false){
 		$dtposted = substr(trim($line), 10);
 	}
    
-   if (trim($line) == '<FITID>' ){
+   	if (trim($line) == '<FITID>' ){
 	   $line = '<FITID>'.$dtposted;
 	   $line .= "\r\n";
 	}   
-   
-   if (trim($line) == '</QFX>'){
+  	// Fix the ending QFX to OFX
+  	if (trim($line) == '</QFX>'){
 	   $line = '</OFX>';
 	   $line .= "\r\n";
 	}
-
+	// Trim DTPOSTED to 8 digits, which is YYYYMMDD only.
 	if (strpos($line, '<DTPOSTED>') !== false){
 		$line = substr($line, strrpos($line, '<DTPOSTED>'), 18);
 		 $line .= "\r\n";
 	}
-	
+	// Trim DTUSER to 8 digits, which is YYYYMMDD only.
 	if (strpos($line, '<DTUSER>') !== false){
 		$line = substr($line, strrpos($line, '<DTUSER>'), 16);
 		 $line .= "\r\n";
 	}
-
+	// Trim DTSTART to 8 digits, which is YYYYMMDD only.
 	if (strpos($line, '<DTSTART>') !== false){
 		$line = substr($line, strrpos($line, '<DTSTART>'), 17);
 		 $line .= "\r\n";
 	}
-	
+	// Trim DTEND to 8 digits, which is YYYYMMDD only.
 	if (strpos($line, '<DTEND>') !== false){
 		$line = substr($line, strrpos($line, '<DTEND>'), 15);
 		 $line .= "\r\n";
